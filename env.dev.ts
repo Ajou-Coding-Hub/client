f87dbd94b2d940c198b548d70642f94c7358cecf
gron1gh1 <gron1gh1@gmail.com>
2022-11-27 04:18:40 +0900

    fix: host

--- env.dev.ts
+++ env.dev.ts
@@ -1,7 +1,7 @@
 const env = Object.freeze({
   "import.meta.env.GOOGLE_CLIENT_ID":
     "'813294973080-4lete6gvepr6hk75gppib6rfpiuig6a8.apps.googleusercontent.com'",
-  "import.meta.env.API_HOST": "'http://localhost:3000'",
+  "import.meta.env.API_HOST": "'https://api.ajou.codes'",
 });
 
 export default env;
