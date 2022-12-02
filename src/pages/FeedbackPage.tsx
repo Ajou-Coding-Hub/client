0a20fc636c8db45d00016615ab2b349db3455927
gron1gh1 <gron1gh1@gmail.com>
2022-12-03 00:31:15 +0900

    feat: virtual infinite scroll

--- src/pages/FeedbackPage.tsx
+++ src/pages/FeedbackPage.tsx
@@ -11,6 +11,8 @@ import React, { useCallback, useMemo, useRef, useState } from "react";
 import { toast } from "react-toastify";
 import TimeAgo from "javascript-time-ago";
 import ko from "javascript-time-ago/locale/ko";
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { classNames } from "@/utils/class";
 TimeAgo.addDefaultLocale(ko);
 const timeAgo = new TimeAgo("ko-KR");
 
@@ -37,35 +39,37 @@ const Feedback: React.FC<IFeedback> = ({
 }) => {
   const ago = useMemo(() => timeAgo.format(new Date(createdAt)), [createdAt]);
   return (
-    <Box className="w-full">
-      <div className="flex gap-5 items-center mb-5">
-        <img className="w-10 h-10 rounded-full" src={owner.picture} />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-5">
+      <Box className="w-full h-full">
+        <div className="flex gap-5 items-center mb-5">
+          <img className="w-10 h-10 rounded-full" src={owner.picture} />
 
-        <div className="flex flex-col gap-1">
-          <div className="text-sm">
-            <p className="text-gray-600">
-              <b className="text-black">{owner.name}</b>님이 피드백을
-              작성했어요.
-            </p>
+          <div className="flex flex-col gap-1">
+            <div className="text-sm">
+              <p className="text-gray-600">
+                <b className="text-black">{owner.name}</b>님이 피드백을
+                작성했어요.
+              </p>
+            </div>
+            <div className="text-xs text-gray-700">아주대학교 학생</div>
           </div>
-          <div className="text-xs text-gray-700">아주대학교 학생</div>
         </div>
-      </div>
 
-      <div className="my-5 whitespace-pre-line">
-        <p>{content}</p>
-      </div>
+        <div className="my-5 whitespace-pre-line">
+          <p className="break-words">{content}</p>
+        </div>
 
-      <p className="text-xs text-gray-600 mb-1">{ago}</p>
+        <p className="text-xs text-gray-600 mb-1">{ago}</p>
 
-      <div
-        onClick={onLike}
-        className="flex gap-1 items-center select-none cursor-pointer w-fit"
-      >
-        <HeartIcon className="w-6 h-6" filled={isLike} />
-        <p className="text-sm">{likeCount}</p>
-      </div>
-    </Box>
+        <div
+          onClick={onLike}
+          className="flex gap-1 items-center select-none cursor-pointer w-fit"
+        >
+          <HeartIcon className="w-6 h-6" filled={isLike} />
+          <p className="text-sm">{likeCount}</p>
+        </div>
+      </Box>
+    </div>
   );
 };
 
@@ -84,29 +88,31 @@ const FeedbackWriter = ({
   }, [onWrite, content]);
 
   return (
-    <div className="w-full mt-3 mb-5">
-      <div className="flex gap-3 items-center mb-3">
-        <img
-          className="w-6 h-6 rounded-full"
-          src={
-            "https://lh3.googleusercontent.com/a/ALm5wu11wN_BukYeweQtfKCh5-yO1Uv7Ndy6nl9GG7cf=s96-c"
-          }
-        />
+    <Padding>
+      <div className="w-full mt-3">
+        <div className="flex gap-3 items-center mb-3">
+          <img
+            className="w-6 h-6 rounded-full"
+            src={
+              "https://lh3.googleusercontent.com/a/ALm5wu11wN_BukYeweQtfKCh5-yO1Uv7Ndy6nl9GG7cf=s96-c"
+            }
+          />
 
-        <div className="flex flex-col gap-1">
-          <p className="text-gray-600 text-sm">
-            <b className="text-black">{"선규"}</b>님의 피드백
-          </p>
+          <div className="flex flex-col gap-1">
+            <p className="text-gray-600 text-sm">
+              <b className="text-black">{"선규"}</b>님의 피드백
+            </p>
+          </div>
+        </div>
+
+        <div className="flex gap-3">
+          <Input value={content} onChange={(e) => setContent(e.target.value)} />
+          <Button onClick={handleWrite} className="w-24 justify-center">
+            작성
+          </Button>
         </div>
       </div>
-
-      <div className="flex gap-3">
-        <Input value={content} onChange={(e) => setContent(e.target.value)} />
-        <Button onClick={handleWrite} className="w-24 justify-center">
-          작성
-        </Button>
-      </div>
-    </div>
+    </Padding>
   );
 };
 
@@ -137,12 +143,13 @@ export function FeedbackPage() {
     fetchPreviousPage,
     hasNextPage,
     hasPreviousPage,
+    isFetching,
     isFetchingNextPage,
     isFetchingPreviousPage,
     refetch,
     ...result
   } = useInfiniteQuery(["feedback"], {
-    queryFn: async ({ pageParam = 0 }) => {
+    queryFn: async ({ pageParam = undefined }) => {
       return (
         await request.get("/feedback", {
           params: {
@@ -162,13 +169,51 @@ export function FeedbackPage() {
 
   const isLike = useCallback(
     (feedbackLikes: Record<"likerId", number>[]) => {
-      return feedbackLikes.find((f) => f.likerId === getUserId());
+      return feedbackLikes?.find((f) => f.likerId === getUserId());
     },
     [getUserId]
   );
 
+  const parentRef = React.useRef(null);
+
+  const allRows = useMemo(() => data?.pages?.flat() ?? [], [data]);
+
+  const rowVirtualizer = useVirtualizer({
+    count: hasNextPage ? allRows.length + 1 : allRows.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: (index) => {
+      return 190;
+    },
+    enableSmoothScroll: false,
+    overscan: 10,
+  });
+
+  React.useEffect(() => {
+    const [lastItem] = [...rowVirtualizer.getVirtualItems()].reverse();
+    if (!lastItem) {
+      return;
+    }
+
+    if (
+      lastItem.index >= allRows.length - 1 &&
+      hasNextPage &&
+      !isFetchingNextPage
+    ) {
+      fetchNextPage();
+    }
+  }, [
+    hasNextPage,
+    fetchNextPage,
+    allRows.length,
+    isFetchingNextPage,
+    rowVirtualizer.getVirtualItems(),
+  ]);
+
   return (
-    <Padding className="flex flex-col gap-3">
+    <div
+      className="h-[calc(100vh_-_64px)] w-full overflow-auto"
+      ref={parentRef}
+    >
       {isLoggedin ? (
         <FeedbackWriter
           onWrite={(content) =>
@@ -176,23 +221,43 @@ export function FeedbackPage() {
           }
         />
       ) : null}
-      {data?.pages?.flat().map((_data: any) => {
-        return (
-          <Feedback
-            key={_data.id}
-            onLike={() => {
-              if (!isLoggedin) {
-                toast.warn("로그인 후 사용 가능합니다.");
-                return;
-              }
-              mutateLike(_data.id);
-            }}
-            {..._data}
-            isLike={isLike(_data?.feedbackLikes)}
-            likeCount={_data._count.feedbackLikes}
-          />
-        );
-      })}
-    </Padding>
+      <div
+        className="w-full relative"
+        style={{
+          height: rowVirtualizer.getTotalSize(),
+        }}
+      >
+        {rowVirtualizer.getVirtualItems().map((virtual) => {
+          const data = allRows[virtual.index];
+          if (!data) {
+            return null;
+          }
+          return (
+            <div
+              key={virtual.key}
+              data-index={virtual.index}
+              ref={rowVirtualizer.measureElement}
+              className={"absolute w-full top-8"}
+              style={{
+                transform: `translateY(${virtual.start}px)`,
+              }}
+            >
+              <Feedback
+                onLike={() => {
+                  if (!isLoggedin) {
+                    toast.warn("로그인 후 사용 가능합니다.");
+                    return;
+                  }
+                  mutateLike(data.id);
+                }}
+                {...data}
+                isLike={isLike(data?.feedbackLikes)}
+                likeCount={data?._count?.feedbackLikes}
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
   );
 }
